b703b148b1e80ec8ace0c1b9aa947419658ca38e
jessikim <jessica.kim17@gmail.com>
2022-11-01 16:19:49 -0500

    complete challenge 3

--- python_functions.py
+++ python_functions.py
@@ -16,3 +16,13 @@ def largest(arr):
 
 # print(largest([1, 2, 3, 4, 0])) -> returns 4
 # print(largest([10, 4, 2, 231, 91, 54])) -> returns 231
+
+# Challenge 3
+
+def occurrences(str1, str2):
+  return str1.count(str2)
+
+# print(occurrences('fleep floop', 'e')) -> returns 2
+# print(occurrences('fleep floop', 'p')) -> returns 2
+# print(occurrences('fleep floop', 'ee')) -> returns 1
+# print(occurrences('fleep floop', 'fe')) -> returns 0
